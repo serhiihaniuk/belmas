a13f13611879fae4074a458632bbd14b77b8e6da
Serhii Haniuk <serhii.haniuk@gmail.com>
2021-08-25 14:46:04 +0200

    change to css native scroll

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,6 @@
-import Swiper from 'swiper/bundle';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.page-wrapper', {
-        // Optional parameters
-        direction: 'vertical',
-        loop: false,
-        cssMode: true,
-        simulateTouch: true,
-    });
 
     const menuItem = document.getElementById("nav-icon3")
     menuItem.addEventListener("click", () => {
