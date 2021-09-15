268334c9432f9f98c569a7ec8816ad863868a88f
Serhii Haniuk <serhii.haniuk@gmail.com>
2021-09-15 15:20:53 +0200

    performance test

--- app/js/app.js
+++ app/js/app.js
@@ -53,49 +53,49 @@ document.addEventListener('DOMContentLoaded', () => {
     const nextSlideBtn = document.querySelector('#slider__next')
     let currentOffset = 0
 
-    prevSlideBtn.addEventListener('click', () => {
-        portfolioSlider.scrollTo({
-            left: currentOffset - portfolioSlideWidth,
-            behavior: 'smooth',
-        })
-    })
-    nextSlideBtn.addEventListener('click', () => {
-        portfolioSlider.scrollTo({
-            left: currentOffset + portfolioSlideWidth,
-            behavior: 'smooth',
-        })
-    })
+    // prevSlideBtn.addEventListener('click', () => {
+    //     portfolioSlider.scrollTo({
+    //         left: currentOffset - portfolioSlideWidth,
+    //         behavior: 'smooth',
+    //     })
+    // })
+    // nextSlideBtn.addEventListener('click', () => {
+    //     portfolioSlider.scrollTo({
+    //         left: currentOffset + portfolioSlideWidth,
+    //         behavior: 'smooth',
+    //     })
+    // })
 
-    const watchSlides = function (entries) {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                currentOffset = entry.target.offsetLeft
-                entry.target.classList.add('active-slide')
-                if (entry.target.offsetLeft === 0) {
-                    prevSlideBtn.style.opacity = '0'
-                    return
-                }
-                if (
-                    entry.target === portfolioSlide[portfolioSlide.length - 1]
-                ) {
-                    nextSlideBtn.style.opacity = '0'
-                    return
-                }
-                nextSlideBtn.style.opacity = '1'
-                prevSlideBtn.style.opacity = '1'
-            } else {
-                entry.target.classList.remove('active-slide')
-            }
-        })
-    }
-    const sliderObserver = new IntersectionObserver(watchSlides, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.6,
-    })
-    portfolioSlide.forEach((slide) => {
-        sliderObserver.observe(slide)
-    })
+    // const watchSlides = function (entries) {
+    //     entries.forEach((entry) => {
+    //         if (entry.isIntersecting) {
+    //             currentOffset = entry.target.offsetLeft
+    //             entry.target.classList.add('active-slide')
+    //             if (entry.target.offsetLeft === 0) {
+    //                 prevSlideBtn.style.opacity = '0'
+    //                 return
+    //             }
+    //             if (
+    //                 entry.target === portfolioSlide[portfolioSlide.length - 1]
+    //             ) {
+    //                 nextSlideBtn.style.opacity = '0'
+    //                 return
+    //             }
+    //             nextSlideBtn.style.opacity = '1'
+    //             prevSlideBtn.style.opacity = '1'
+    //         } else {
+    //             entry.target.classList.remove('active-slide')
+    //         }
+    //     })
+    // }
+    // const sliderObserver = new IntersectionObserver(watchSlides, {
+    //     root: null,
+    //     rootMargin: '0px',
+    //     threshold: 0.6,
+    // })
+    // portfolioSlide.forEach((slide) => {
+    //     sliderObserver.observe(slide)
+    // })
 
     const portfolioImages = document.body.querySelectorAll(
         '.portfolio-page__img--big'
