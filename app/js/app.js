6e433a78eecc783ce47cdf4163db979a377a554d
shaniuk <serhii.haniuk@ringieraxelspringer.pl>
2021-09-29 21:28:10 +0200

    add lazy load for portfolio page

--- app/js/app.js
+++ app/js/app.js
@@ -1,133 +1,136 @@
+import { portfolioPage } from './lazyHtml';
+
 document.addEventListener('DOMContentLoaded', () => {
-    //utils
+	//utils
+	const trottle = (callback) => {
+		let wait = false;
+		return (e) => {
+			if (wait) return;
+			wait = true;
+			callback(e);
+			setTimeout(() => (wait = false), 8);
+		};
+	};
+	//utils
 
-    const throttle = (callback, timeout) => {
-        let wait = false
-        return (e) => {
-            if (wait) return
-            wait = true
-            callback(e)
-            setTimeout(() => (wait = false), timeout)
-        }
-    }
+	//main
+	const menuItem = document.getElementById('nav-icon3');
+	const menu = document.querySelector('.menu');
+	menuItem.addEventListener('click', () => {
+		menuItem.classList.toggle('open');
+		menu.classList.toggle('open');
+	});
 
-    //utils
+	//menu links
+	const menuLinks = document.querySelectorAll('.menu__item');
+	menuLinks.forEach((link) => {
+		link.addEventListener('click', () => {
+			menu.classList.remove('open');
+			menuItem.classList.remove('open');
+		});
+	});
+	// book popup
+	const popupWrapper = document.querySelector('.book');
+	const runBookButtons = () => {
+		const bookButtons = document.querySelectorAll('.open-popup');
+		bookButtons.forEach((bookButton) => {
+			bookButton.addEventListener('click', () => {
+				popupWrapper.classList.add('open');
+			});
+		});
+	};
 
-    //main
-    const menuItem = document.getElementById('nav-icon3')
-    const menu = document.querySelector('.menu')
-    menuItem.addEventListener('click', () => {
-        menuItem.classList.toggle('open')
-        menu.classList.toggle('open')
-    })
+	popupWrapper.addEventListener('click', (e) => {
+		if (e.target === popupWrapper) {
+			popupWrapper.classList.remove('open');
+		}
+	});
 
-    //menu links
-    const menuLinks = document.querySelectorAll('.menu__item')
-    menuLinks.forEach((link) => {
-        link.addEventListener('click', () => {
-            menu.classList.remove('open')
-            menuItem.classList.remove('open')
-        })
-    })
-    // book popup
-    const popupWrapper = document.querySelector('.book')
-    const bookButtons = document.querySelectorAll('.open-popup')
-    bookButtons.forEach((bookButton) => {
-        bookButton.addEventListener('click', () => {
-            popupWrapper.classList.add('open')
-        })
-    })
+	//slider
+	const runPortfolioSlider = () => {
+		const portfolioSlider = document.querySelector('.portfolio-page__slider');
+		const portfolioSlide = document.querySelectorAll('.portfolio-page__slide');
+		const portfolioSlideWidth = portfolioSlide[0].offsetWidth;
+		const prevSlideBtn = document.querySelector('#slider__prev');
+		const nextSlideBtn = document.querySelector('#slider__next');
+		let currentOffset = 0;
 
-    popupWrapper.addEventListener('click', (e) => {
-        if (e.target === popupWrapper) {
-            popupWrapper.classList.remove('open')
-        }
-    })
+		prevSlideBtn.addEventListener('click', () => {
+			portfolioSlider.scrollTo({ left: currentOffset - portfolioSlideWidth, behavior: 'smooth' });
+		});
+		nextSlideBtn.addEventListener('click', () => {
+			portfolioSlider.scrollTo({ left: currentOffset + portfolioSlideWidth, behavior: 'smooth' });
+		});
 
-    //slider
+		const watchSlides = function (entries) {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					currentOffset = entry.target.offsetLeft;
+					entry.target.classList.add('active-slide');
+					if (entry.target.offsetLeft === 0) {
+						prevSlideBtn.style.opacity = '0';
+						return;
+					}
+					if (entry.target === portfolioSlide[portfolioSlide.length - 1]) {
+						nextSlideBtn.style.opacity = '0';
+						return;
+					}
+					nextSlideBtn.style.opacity = '1';
+					prevSlideBtn.style.opacity = '1';
+				} else {
+					entry.target.classList.remove('active-slide');
+				}
+			});
+		};
+		const sliderObserver = new IntersectionObserver(watchSlides, {
+			root: null,
+			rootMargin: '0px',
+			threshold: 0.6
+		});
+		portfolioSlide.forEach((slide) => {
+			sliderObserver.observe(slide);
+		});
+	};
 
-    const portfolioSlider = document.querySelector('.portfolio-page__slider')
-    const portfolioSlide = document.querySelectorAll('.portfolio-page__slide')
-    const portfolioSlideWidth = portfolioSlide[0].offsetWidth
-    const prevSlideBtn = document.querySelector('#slider__prev')
-    const nextSlideBtn = document.querySelector('#slider__next')
-    let currentOffset = 0
+    //price component
+	const priceElement = document.querySelector('.price');
+	const priceScrollbar = document.querySelector('.price__scrollbar');
+	const priceScrollbarThumb = document.querySelector('.price__thumb');
+	let scrollHeight;
 
-    prevSlideBtn.addEventListener('click', () => {
-        portfolioSlider.scrollTo({
-            left: currentOffset - portfolioSlideWidth,
-            behavior: 'smooth',
-        })
-    })
-    nextSlideBtn.addEventListener('click', () => {
-        portfolioSlider.scrollTo({
-            left: currentOffset + portfolioSlideWidth,
-            behavior: 'smooth',
-        })
-    })
+	const scrollResizeHandler = () => {
+		priceScrollbar.style.height = priceElement.clientHeight + 'px';
+		scrollHeight = priceElement.scrollHeight - priceElement.clientHeight;
+	};
+	scrollResizeHandler();
+	window.addEventListener('resize', scrollResizeHandler);
 
-    const watchSlides = function (entries) {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                currentOffset = entry.target.offsetLeft
-                entry.target.classList.add('active-slide')
-                if (entry.target.offsetLeft === 0) {
-                    prevSlideBtn.style.opacity = '0'
-                    return
-                }
-                if (
-                    entry.target === portfolioSlide[portfolioSlide.length - 1]
-                ) {
-                    nextSlideBtn.style.opacity = '0'
-                    return
-                }
-                nextSlideBtn.style.opacity = '1'
-                prevSlideBtn.style.opacity = '1'
-            } else {
-                entry.target.classList.remove('active-slide')
-            }
-        })
-    }
-    const sliderObserver = new IntersectionObserver(watchSlides, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.6,
-    })
-    portfolioSlide.forEach((slide) => {
-        sliderObserver.observe(slide)
-    })
+	priceElement.addEventListener(
+		'scroll',
+		trottle((e) => {
+			const scrollTop = e.target.scrollTop;
+			const scrollPercent = parseInt((scrollTop / scrollHeight) * 100);
+			priceScrollbarThumb.style.top = scrollPercent + '%';
+			priceScrollbarThumb.style.transform = `translateY(-${scrollPercent}%)`;
+		})
+	);
 
-    const portfolioImages = document.body.querySelectorAll(
-        '.portfolio-page__img--big'
-    )
 
-    setTimeout(() => {
-        portfolioImages.forEach((img) => {
-            img.src = img.dataset.href
-        })
-    }, 500)
+	//lazyLoad
+	setTimeout(() => {
+		const portfolioSectionDiv = document.body.querySelector('.portfolio-page__wrapper');
+		portfolioSectionDiv.insertAdjacentHTML('afterbegin', portfolioPage);
+		runPortfolioSlider();
+		insertPortfolioImages();
+        runBookButtons()
+	}, 200);
 
-    const priceElement = document.querySelector('.price')
-    const priceScrollbar = document.querySelector('.price__scrollbar')
-    const priceScrollbarThumb = document.querySelector('.price__thumb')
-    let scrollHeight
-
-    const scrollResizeHandler = () => {
-        setTimeout(() => {
-            priceScrollbar.style.height = priceElement.clientHeight + 'px'
-            scrollHeight = priceElement.scrollHeight - priceElement.clientHeight
-        }, 30)
-    }
-    scrollResizeHandler()
-    window.addEventListener('resize', scrollResizeHandler)
-
-    priceElement.addEventListener(
-        'scroll',
-        throttle((e) => {
-            const scrollTop = e.target.scrollTop
-            const scrollPercent = parseInt((scrollTop / scrollHeight) * 100)
-            priceScrollbarThumb.style.top = scrollPercent + '%'
-            priceScrollbarThumb.style.transform = `translateY(-${scrollPercent}%)`
-        }, 5)
-    )
-})
+	const insertPortfolioImages = () => {
+		setTimeout(() => {
+			const portfolioImages = document.body.querySelectorAll('.portfolio-page__img--big');
+			portfolioImages.forEach((img) => {
+				img.src = img.dataset.href;
+			});
+		}, 100);
+	};
+});
---
+++ app/js/lazyHtml.js
@@ -0,0 +1,115 @@
+export const portfolioPage =  `
+
+<div class="portfolio-page__slider">
+        <div class="portfolio-page__slide 1">
+            <div class="portfolio-page__gallery">
+                <div class="portfolio-page__img-wrapper--big">
+                    <img src="#" data-href="images/dist/port2.webp" alt="portfolio image" class="portfolio-page__img--big">
+                </div>
+            </div>
+
+            <div class="portfolio-page__description portfolio-page__description--main">
+                <h2 class="portfolio-page__title">Naszę portfolio</h2>
+                <div class="portfolio-page__text">
+                    <div>Kobiecość przejawia się w szczegółach, a jednym z nich jest manicure</div>
+                    <div class="portfolio-page__map">
+                        <img src="images/dist/map.svg" alt="map ico belmas"
+                             class="social__img ico-ring-animation">
+
+                        Wrocław
+                        <br/>
+                        Pomorska 32<br/>
+                        Blisko centrum<br/>
+                        Free parking<br/>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="portfolio-page__slide 2">
+            <div class="portfolio-page__gallery">
+                <div class="portfolio-page__img-wrapper--big">
+                    <img src="#" data-href="images/dist/port2.webp" alt="portfolio image" class="portfolio-page__img--big">
+                </div>
+            </div>
+
+            <div class="portfolio-page__description">
+                <h2 class="portfolio-page__title">2 portfolio</h2>
+                <p class="portfolio-page__text">Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem
+                    Ipsum has been the industry's standard dummy text </p>
+            </div>
+
+        </div>
+        <div class="portfolio-page__slide 3">
+            <div class="portfolio-page__gallery">
+                <div class="portfolio-page__img-wrapper--big">
+                    <img src="#" data-href="images/dist/port2.webp" alt="portfolio image" class="portfolio-page__img--big">
+                </div>
+            </div>
+
+            <div class="portfolio-page__description">
+                <h2 class="portfolio-page__title">3 portfolio</h2>
+                <p class="portfolio-page__text">Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem
+                    Ipsum has been the industry's standard dummy text </p>
+
+
+            </div>
+
+        </div>
+        <div class="portfolio-page__slide 4">
+            <div class="portfolio-page__gallery">
+                <div class="portfolio-page__img-wrapper--big">
+                    <img src="#" data-href="images/dist/port2.webp" alt="portfolio image" class="portfolio-page__img--big">
+                </div>
+            </div>
+
+            <div class="portfolio-page__description">
+                <h2 class="portfolio-page__title">4 portfolio</h2>
+                <p class="portfolio-page__text">Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem
+                    Ipsum has been the industry's standard dummy text </p>
+
+
+            </div>
+
+        </div>
+        <div class="portfolio-page__slide 5">
+            <div class="portfolio-page__gallery">
+                <div class="portfolio-page__img-wrapper--big">
+                    <img src="#" data-href="images/dist/port2.webp" alt="portfolio image" class="portfolio-page__img--big">
+                </div>
+            </div>
+
+            <div class="portfolio-page__description">
+                <h2 class="portfolio-page__title">5 portfolio</h2>
+                <p class="portfolio-page__text">Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem
+                    Ipsum has been the industry's standard dummy text </p>
+            </div>
+
+        </div>
+    </div>
+    <img src="images/dist/bg-item-pink.svg" class="portfolio-page__bg portfolio-page__bg--top bg-move-animation"
+         alt="bg">
+    <img src="images/dist/bg-item-pink.svg" class="portfolio-page__bg portfolio-page__bg--bot bg-move-animation"
+         alt="bg">
+
+    <div class="portfolio-page__controls">
+        <button class="btn-arrow" id="slider__prev">
+            <img src="images/dist/goNext.svg" alt="next slide" class="brn-arrow__img brn-arrow__img--left">
+        </button>
+        <button class="btn-arrow " id="slider__next">
+            <img src="images/dist/goNext.svg" alt="next slide" class="brn-arrow__img brn-arrow__img--right">
+        </button>
+    </div>
+    <div class="portfolio-page__next-page">
+        <button class="btn-main portfolio-page__order-btn open-popup">Zarezerwuj wizytę</button>
+        <button class="btn-arrow fadeIn">
+            <img src="images/dist/goNext.svg" alt="next slide" class="btn-arrow__img">
+        </button>
+    </div>
+
+
+`
